39edbeec008a136eb05df24119610b5443797774
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-19 00:45:18 +0200

    add

---
+++ 001.IntroProgramming/002.GoodDay/GoodDay.cs
@@ -0,0 +1,11 @@
+﻿//Chapter Intro programming, Task 005
+//18.11.2022, 00:52
+using System;
+
+public class GoodDay
+{
+    static void Main()
+    {
+        Console.WriteLine("Good day!");
+    }
+}
