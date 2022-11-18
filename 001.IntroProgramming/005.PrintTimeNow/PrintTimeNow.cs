3ef46b53ffec4e65727a35da6f3d99f6d8a4ae5a
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-19 00:46:14 +0200

    add

---
+++ 001.IntroProgramming/005.PrintTimeNow/PrintTimeNow.cs
@@ -0,0 +1,11 @@
+﻿//Chapter Intro programming, Task 008
+//18.11.2022, 00:56
+using System;
+
+public class PrintTimeNow
+{
+    static void Main()
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+}
