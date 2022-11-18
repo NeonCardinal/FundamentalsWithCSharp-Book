2236cc5aa7178436118505b178b74cd3b723a381
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-19 00:46:46 +0200

    add

---
+++ 001.IntroProgramming/007.PrintSerie/PrintSerie.cs
@@ -0,0 +1,22 @@
+﻿//Chapter Intro programming, Task 010
+//18.11.2022, 00:59
+using System;
+using System.Collections.Concurrent;
+
+public class PrintSerie
+{
+    static void Main()
+    {
+        for(int i = 2; i <= 102; i++)
+        {
+            if(i % 2 == 0)
+            {
+                Console.WriteLine($"{i}");
+            }
+            else
+            {
+                Console.WriteLine($"-{i}");
+            }
+        }
+    }
+}
