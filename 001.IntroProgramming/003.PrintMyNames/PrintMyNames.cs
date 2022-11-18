60b69b45f64e158d283ed8ca215148d8f34fd5c9
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-19 00:45:41 +0200

    add

---
+++ 001.IntroProgramming/003.PrintMyNames/PrintMyNames.cs
@@ -0,0 +1,11 @@
+﻿//Chapter Intro programming, Task 006
+//18.11.2022, 00:54
+using System;
+
+public class PrintMyNames
+{
+    static void Main()
+    {
+        Console.WriteLine("Nikolay Naydenov");
+    }
+}
