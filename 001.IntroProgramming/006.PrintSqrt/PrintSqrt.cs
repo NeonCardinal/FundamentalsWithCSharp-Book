cf5b34c94210e4b13a1f55c6d63ece2ba597ecf4
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-19 00:46:30 +0200

    add

---
+++ 001.IntroProgramming/006.PrintSqrt/PrintSqrt.cs
@@ -0,0 +1,11 @@
+﻿//Chapter Intro programming, Task 009
+//18.11.2022, 00:57
+using System;
+
+public class PrintSqrt
+{
+    static void Main()
+    {
+        Console.WriteLine(Math.Sqrt(12345));
+    }
+}
