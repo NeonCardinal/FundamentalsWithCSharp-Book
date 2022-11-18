faf8868ee50d3a987872f68dda57d6957d43671f
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-19 00:44:26 +0200

    add

---
+++ 001.IntroProgramming/001.HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,11 @@
+﻿//Chapter Intro programming, Task 004
+//18.11.2022, 00:51
+using System;
+
+public class HelloCSharp
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, C#!");
+    }
+}
