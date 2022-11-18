64b4609a6ae58a0245f97786e64cb1c61d19cf39
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-19 00:45:58 +0200

    add

---
+++ 001.IntroProgramming/004.PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,13 @@
+﻿//Chapter Intro programming, Task 007
+//18.11.2022, 00:55
+using System;
+
+public class PrintNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine(1);
+        Console.WriteLine(101);
+        Console.WriteLine(1001);
+    }
+}
