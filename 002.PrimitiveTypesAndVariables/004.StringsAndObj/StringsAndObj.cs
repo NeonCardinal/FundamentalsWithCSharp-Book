f4d66ad9b3f827abca3ea638fb1cd38904bc8c06
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-23 00:44:44 +0200

    add

---
+++ 002.PrimitiveTypesAndVariables/004.StringsAndObj/StringsAndObj.cs
@@ -0,0 +1,19 @@
+﻿//Chapter Primitive types and variables - book, Task 007
+//22.11.2022, 23:21
+using System;
+
+public class StringsAndObj
+{
+    static void Main()
+    {
+        string first = "Hello";
+        string second = "World";
+        object firstObj = first + " " + second;
+        string third = (string)firstObj;
+
+        Console.Write("This is object: ");
+        Console.WriteLine(firstObj);
+        Console.Write("This is string: ");
+        Console.WriteLine(third);
+    }
+}
