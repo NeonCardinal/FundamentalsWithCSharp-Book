a54e12282b8af1bee4d0229d77fd842d94201b19
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-23 00:44:10 +0200

    add

---
+++ 002.PrimitiveTypesAndVariables/002.DeclareChar/DeclareChar.cs
@@ -0,0 +1,13 @@
+﻿//Chapter Primitive types and variables - book, Task 005
+//22.11.2022, 23:16
+using System;
+
+public class DeclareChar
+{
+    static void Main()
+    {
+        char symbol = '\u0048';
+
+        Console.WriteLine(symbol);
+    }
+}
