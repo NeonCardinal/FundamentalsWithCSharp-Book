f2c963059ec2b2a0e4ab6b1c7fe83f1660bd3b2d
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-23 00:43:50 +0200

    add

---
+++ 002.PrimitiveTypesAndVariables/001.HexInit/HexInit.cs
@@ -0,0 +1,13 @@
+﻿//Chapter Primitive types and variable - book, Task 004
+//22.11.2022, 23:14
+using System;
+
+public class HexInit
+{
+    static void Main()
+    {
+        int hex = 0x100;
+
+        Console.WriteLine(hex);
+    }
+}
