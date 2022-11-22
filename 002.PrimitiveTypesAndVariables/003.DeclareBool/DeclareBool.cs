4ef887132191efb01636988ff0ce8799ecf9f5b6
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-23 00:44:27 +0200

    add

---
+++ 002.PrimitiveTypesAndVariables/003.DeclareBool/DeclareBool.cs
@@ -0,0 +1,13 @@
+﻿//Chapter Primitive types and variables - book, Task 006
+//22.11.2022, 23:19
+using System;
+
+public class DeclareBool
+{
+    static void Main()
+    {
+        bool isMale = true;
+
+        Console.WriteLine(isMale);
+    }
+}
