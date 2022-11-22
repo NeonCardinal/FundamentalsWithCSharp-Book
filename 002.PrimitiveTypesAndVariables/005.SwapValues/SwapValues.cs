2b0f6f49e722047dfd169980e45edef922f02081
NeonCardinal <nvnnaidenov@gmail.com>
2022-11-23 00:45:01 +0200

    add

---
+++ 002.PrimitiveTypesAndVariables/005.SwapValues/SwapValues.cs
@@ -0,0 +1,22 @@
+﻿//Chapter Primitive types and variable - book, Task 013
+//22.11.2022, 23:24
+using System;
+
+public class SwapValues
+{
+    static void Main()
+    {
+        Console.Write("a = ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Before swap: a = {a}, b = {b}");
+
+        int c = a;
+        a = b;
+        b = c;
+
+        Console.WriteLine($"After swapping: a = {a}, b = {b}");
+    }
+}
