856c43f564b2cd080dc0db58733d6a49f0f3688e
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-11 00:43:11 +0200

    add

---
+++ 003.OperatorsAndExpressions/007.MoonWeight/MoonWeight.cs
@@ -0,0 +1,17 @@
+﻿//Chapter Operators and expressions - book, Task 007
+//11.12.2022, 00:32
+using System;
+
+public class MoonWeight
+{
+    static void Main()
+    {
+        Console.Write("Въведете вашето тегло: ");
+        int earthWeight = int.Parse(Console.ReadLine());
+
+        double moonWeight = earthWeight * 0.17;
+
+        Console.Write("Вашето тегло на луната ще е: ");
+        Console.WriteLine(moonWeight);
+    }
+}
