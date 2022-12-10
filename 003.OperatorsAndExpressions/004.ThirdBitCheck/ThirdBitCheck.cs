74d7de394faa8d3b5b2460fbf76201cda1a84ddf
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-11 00:42:23 +0200

    add

---
+++ 003.OperatorsAndExpressions/004.ThirdBitCheck/ThirdBitCheck.cs
@@ -0,0 +1,16 @@
+﻿//Chapter Operators and expressions - book, Task 004
+//10.12.2022, 23:53
+using System;
+
+public class ThirdBitCheck
+{
+    static void Main()
+    {
+        Console.Write("Въведете число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isOne = (number & 8) != 0;
+
+        Console.WriteLine(isOne);
+    }
+}
