11f75a086c650b3d5708ada8495681af5ef972af
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-11 00:42:07 +0200

    add

---
+++ 003.OperatorsAndExpressions/003.IsSeven/IsSeven.cs
@@ -0,0 +1,18 @@
+﻿//Chapter Operators and expressions - book, Task 003
+//10.12.2022, 23:49
+using System;
+
+public class IsSeven
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int a = number / 100;
+        int b = a % 10;
+        bool isThirdDigitEqSeven = b == 7;
+
+        Console.WriteLine(isThirdDigitEqSeven);       
+    }
+}
