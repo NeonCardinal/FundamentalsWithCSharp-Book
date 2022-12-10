c7045986286c5c3ced4d5ed2ac33ba8ba6b72a28
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-11 00:41:37 +0200

    add

---
+++ 003.OperatorsAndExpressions/001.OddOrEven/OddOrEven.cs
@@ -0,0 +1,18 @@
+﻿//Chapter Operators and expressions - book, Task 001
+//10.12.2022, 23:41
+using System;
+
+public class OddOrEven
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        bool isOdd = number % 2 != 0;
+
+        if(isOdd)
+            Console.WriteLine("odd");
+        else
+            Console.WriteLine("even");
+    }
+}
