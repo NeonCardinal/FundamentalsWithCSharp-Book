3a2b7f1ae2933504ef798572d7931a725b374d3e
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-11 00:41:52 +0200

    add

---
+++ 003.OperatorsAndExpressions/002.DivideBySeven/DivideBySeven.cs
@@ -0,0 +1,17 @@
+﻿//Chapter Operators and expressions - book, Task 002
+//10.12.2022, 23:46
+using System;
+using System.Diagnostics;
+
+public class DivideBySeven
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isDivided = number % 5 == 0 && number % 7 == 0;
+
+        Console.WriteLine(isDivided);
+    }
+}
