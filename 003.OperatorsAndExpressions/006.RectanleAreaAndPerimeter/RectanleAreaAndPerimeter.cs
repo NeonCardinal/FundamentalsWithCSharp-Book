7263496d13cf508488337973fcaa0a5e6b38fda7
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-11 00:42:55 +0200

    add

---
+++ 003.OperatorsAndExpressions/006.RectanleAreaAndPerimeter/RectanleAreaAndPerimeter.cs
@@ -0,0 +1,20 @@
+﻿//Chapter Operators and expressions - book, Task 006
+//11.12.2022, 00:29
+using System;
+
+public class RectanleAreaAndPerimeter
+{
+    static void Main()
+    {
+        Console.Write("a = ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        int b = int.Parse(Console.ReadLine());
+
+        int perimeter = (a + b) * 2;
+        int area = a * b;
+
+        Console.WriteLine($"Perimeter = {perimeter}");
+        Console.WriteLine($"Area = {area}");
+    }
+}
