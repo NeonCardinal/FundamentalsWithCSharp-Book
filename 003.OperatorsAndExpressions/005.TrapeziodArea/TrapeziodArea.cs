a841a4ec4995132b16cebbc12fa7b60cac53eafb
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-11 00:42:39 +0200

    add

---
+++ 003.OperatorsAndExpressions/005.TrapeziodArea/TrapeziodArea.cs
@@ -0,0 +1,17 @@
+﻿//Chapter Operators and expressions - book, Task 005
+//10.12.2022, 23:57
+using System;
+
+public class TrapeziodArea
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+
+        double area = (a + b) * h / 2;
+
+        Console.WriteLine(area);
+    }
+}
