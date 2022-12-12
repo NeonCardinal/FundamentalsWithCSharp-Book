4a775796f8099a04431f29316ac809ba4c7df7ca
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-13 00:56:01 +0200

    add

---
+++ 005.ConditionalStatements/005.PlayWithSwitch/PlayWithSwitch.cs
@@ -0,0 +1,36 @@
+﻿//Chapter Conditional statements - book, Task 008
+//13.12.2022, 00:48
+using System;
+
+public class PlayWithSwitch
+{
+    static void Main()
+    {
+        string command = Console.ReadLine();
+
+        switch(command)
+        {
+            case "int":
+                {
+                    int a = int.Parse(Console.ReadLine());
+                    Console.WriteLine(a + 1);
+                }
+                break;
+            case "double":
+                {
+                    double d = double.Parse(Console.ReadLine());
+                    Console.WriteLine(d + 1.00);
+                }
+                break;
+            case "string":
+                {
+                    string str = Console.ReadLine();
+                    Console.WriteLine(str + "+");
+                }
+                break;
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
+    }
+}
