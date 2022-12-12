615c2cff47807a46895aad76be0e0af76f78179d
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-13 00:55:11 +0200

    add

---
+++ 005.ConditionalStatements/002.PrintSign/PrintSign.cs
@@ -0,0 +1,49 @@
+﻿//Chapter Contitional statements - book, Task 002
+//13.12.2022, 00:14
+using System;
+
+public class PrintSign
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int cnt = 0;
+
+        if(a < 0)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt--;
+        }
+        if(b < 0)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt--;
+        }
+        if(c < 0)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt--;
+        }
+
+        if(cnt % 2 == 0)
+        {
+            Console.WriteLine("+");
+        }
+        else
+        {
+            Console.WriteLine("-");
+        }
+    }
+}
