5901e980cc29420fe20d18a893ae8da16f8556a1
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-13 00:55:26 +0200

    add

---
+++ 005.ConditionalStatements/003.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,62 @@
+﻿//Chapter Conditional statements - book, Task 003
+//13.12.2022, 00:18
+using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int greater = 0;
+
+        if(a > b)
+        {
+            greater = a;
+        }
+        else
+        {
+            greater = b;
+        }
+
+        if(a > c)
+        {
+            greater = a;
+        }
+        else
+        {
+            greater = c;
+        }
+
+        if(b > c)
+        {
+            greater = b;
+        }
+        else
+        {
+            greater = c;
+        }
+
+        if(b > a)
+        {
+            greater = b;
+        }
+        else
+        {
+            greater = a;
+        }
+
+        if(c > b)
+        {
+            greater = c;
+        }
+        else
+        {
+            greater = b;
+        }
+
+        Console.WriteLine($"Greater number is: {greater}");
+    }
+}
