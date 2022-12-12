22441ab5ef87e8f1551ac92944e8118fd5f604e6
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-12 21:25:43 +0200

    add

---
+++ 004.ConsoleInputAndOutput/005.PrintMaxNumber/PrintMaxNumber.cs
@@ -0,0 +1,14 @@
+﻿//Chapter Console input and output - book, Task 006
+//11.12.2022, 01:34
+using System;
+
+public class PrintMaxNumber
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Greater number is: {Math.Max(a, b)}");
+    }
+}
