852f8448711ee54ead37b8cf6ee854df616fbc83
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-12 21:27:11 +0200

    add

---
+++ 004.ConsoleInputAndOutput/008.ReadNNumbersAndPrintSum/ReadNNumbersAndPrintSum.cs
@@ -0,0 +1,22 @@
+﻿//Chapter Console input and output - book, Task 009
+//11.12.2022, 01:45
+using System;
+
+public class ReadNNumbersAndPrintSum
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
