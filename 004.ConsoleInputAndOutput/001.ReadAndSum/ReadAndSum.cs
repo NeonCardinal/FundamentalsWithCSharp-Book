45b7191e9a51ad3fb839824a166072ad41f862a4
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-12 21:24:34 +0200

    add

---
+++ 004.ConsoleInputAndOutput/001.ReadAndSum/ReadAndSum.cs
@@ -0,0 +1,17 @@
+﻿//Chapter Console input and output - book, Task 001
+//11.12.2022, 01:15
+using System;
+
+public class ReadAndSum
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int sum = a + b + c;
+
+        Console.WriteLine(sum);
+    }
+}
