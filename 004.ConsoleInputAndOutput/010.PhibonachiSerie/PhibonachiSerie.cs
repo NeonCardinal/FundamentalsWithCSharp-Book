d91b9d5159edb37feb390eaed3704234494305a8
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-12 21:27:56 +0200

    add

---
+++ 004.ConsoleInputAndOutput/010.PhibonachiSerie/PhibonachiSerie.cs
@@ -0,0 +1,21 @@
+﻿//Chapter Console input and output - book, Task 011
+//11.12.2022, 02:26
+using System;
+
+public class PhibonachiSerie
+{
+    static void Main()
+    {
+        long fibb = 0;
+        long a = 0;
+        long b = 1;
+
+        for(int i = 1; i <= 100; i++)
+        {
+            fibb = a + b;
+            a = b;
+            b = fibb;
+            Console.WriteLine(fibb);
+        }
+    }
+}
