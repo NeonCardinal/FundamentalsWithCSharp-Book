b71a0a4b624a122582cc3acb202602193db57365
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-12 21:25:12 +0200

    add

---
+++ 004.ConsoleInputAndOutput/003.CompanyInfo/CompanyInfo.cs
@@ -0,0 +1,23 @@
+﻿//Chapter Console input and output - book, Task 003
+//11.12.2022, 01:22
+using System;
+
+public class CompanyInfo
+{
+    static void Main()
+    {
+        string companyName = Console.ReadLine();
+        string companyAddress = Console.ReadLine();
+        int companyPhone = int.Parse(Console.ReadLine());
+        int companyFax = int.Parse(Console.ReadLine());
+        string companyWeb = Console.ReadLine();
+        string managerFirstName = Console.ReadLine();
+        string managerLastName = Console.ReadLine();
+        int managerPhone = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Company name: {companyName}, company address: {companyAddress}," +
+            $" company phone: {companyPhone}, company fax: {companyFax}, company web: {companyWeb}");
+        Console.WriteLine($"Manager first name: {managerFirstName}, manager last name: {managerLastName}, " +
+            $"manager phone number: {managerPhone}");
+    }
+}
