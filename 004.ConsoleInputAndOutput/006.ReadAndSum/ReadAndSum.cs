bce648f3fb69974a19c2528e03d1f993356b47bf
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-12 21:26:01 +0200

    add

---
+++ 004.ConsoleInputAndOutput/006.ReadAndSum/ReadAndSum.cs
@@ -0,0 +1,27 @@
+﻿//Chapter Console input and output - book, Task 007
+//11.12.2022, 01:37
+using System;
+
+public class ReadAndSum
+{
+    static void Main()
+    {
+        int sum = 0;
+        int number;
+
+        for(int i = 0; i < 5; i++)
+        {
+            if(int.TryParse(Console.ReadLine(), out number) != true)
+            {
+                Console.WriteLine("Wrong value! Try again!");
+                i--;
+            }
+            else
+            {
+                sum += number;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
