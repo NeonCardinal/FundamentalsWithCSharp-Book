234be76d1f9676bc00934b05be1bb234867f62fc
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-12 21:26:50 +0200

    add

---
+++ 004.ConsoleInputAndOutput/007.GreaterBetweenFiveNumbers/GreaterBetweenFiveNumbers.cs
@@ -0,0 +1,23 @@
+﻿//Chapter Console input and output - book, Task 008
+//11.12.2022, 01:42
+using System;
+
+public class GreaterBetweenFiveNumbers
+{
+    static void Main()
+    {
+        int greater = int.MinValue;
+
+        for(int i = 0; i < 5; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number >= greater)
+            {
+                greater = number;
+            }
+        }
+
+        Console.WriteLine(greater);
+    }
+}
