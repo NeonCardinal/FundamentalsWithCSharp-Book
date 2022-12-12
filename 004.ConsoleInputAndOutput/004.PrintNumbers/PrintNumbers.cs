43f91519091b7d455c844b1dd364b8eb63a5e962
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-12 21:25:28 +0200

    add

---
+++ 004.ConsoleInputAndOutput/004.PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,20 @@
+﻿//Chapter Console input and output - book, Task 005
+//11.12.2022, 01:31
+using System;
+
+public class PrintNumbers
+{
+    static void Main()
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        for(int i = start; i <= end; i++)
+        {
+            if(i % 5 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
