2b1baf68c25f0d3f84f594c6553186919eacbbb3
NeonCardinal <nvnnaidenov@gmail.com>
2022-12-14 00:00:46 +0200

    add

---
+++ 006.Loops/001.PrintNNumbers/PrintNNumbers.cs
@@ -0,0 +1,16 @@
+﻿//Chapter Loops - book, Task 001
+//13.12.2022, 23:47
+using System;
+
+public class PrintNNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
